047de82246e3649db088c15b46e9ae4c1b9f4b0a
南海十三郎 <lmajang666@outlook.com>
2023-05-28 01:36:54 +0800

    test

---
+++ WebBack/src/test/java/com/example/webback/WebBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
